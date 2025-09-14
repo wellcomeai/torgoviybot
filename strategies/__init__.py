2fd2f998572cbd77809824e4b67c2ebbd64a97bf
wellcomeai <19wellai96@gmail.com>
2025-09-14 15:45:08 +0300

    Create __init__.py

---
+++ strategies/__init__.py
@@ -0,0 +1,8 @@
+"""
+Торговые стратегии
+Базовые классы и реализации стратегий
+"""
+
+from .base_strategy import BaseStrategy, TradingSignal, TechnicalIndicators
+
+__all__ = ["BaseStrategy", "TradingSignal", "TechnicalIndicators"]
