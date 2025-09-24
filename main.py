10dc47a7dd53aea32ea825a8099cf1f4c7b37a79
wellcomeai <19wellai96@gmail.com>
2025-09-24 13:31:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from config.settings import Settings, get_settings
 
 # ИСПРАВЛЕНО: импортируем новый PybitWebSocketManager
-from core.pybit_websocket_manager import PybitWebSocketManager as WebSocketManager
+from core import WebSocketManager
 
 from strategies.base_strategy import BaseStrategy
 from telegram_bot.bot import TelegramBot
